b48b3f2eb26c860b6836f749e63c6ebbf6f69ef7
hikmiau <166746547+hikmiau@users.noreply.github.com>
2026-07-15 21:34:38 -0300

    Aula POO 01

---
+++ MeuPoo01/MeuPoo01/Bola.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPoo01
+{
+    internal class Bola
+    {
+        public int Id;
+        public string nome;
+        public string material;
+        public string cor;
+
+        public Bola(int id, string nome, string material)
+        {
+            this.Id = id;
+            this.nome = nome;
+            this.material = material;
+            this.cor = "vermelho";
+        }
+
+        public void MostrarDados()
+        {
+            Console.WriteLine("Nome: " + this.nome);
+            Console.WriteLine("Material: " + this.material);
+            Console.WriteLine("Cor: " + this.cor);
+        }
+    }
+}
---
+++ MeuPoo01/MeuPoo01/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPoo01
+{
+    internal class Cliente
+    {
+        public int Id;
+        public string Name;
+        public double Altura;
+        public string Cpf;
+
+        public Cliente(int id, string nome, string cpf)
+        {
+            this.Id = id;
+            this.Name = nome;
+            this.Cpf = cpf;
+        }
+
+        
+    }
+}
