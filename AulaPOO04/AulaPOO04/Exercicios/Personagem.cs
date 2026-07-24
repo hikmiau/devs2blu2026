26a533d35fd9db1f51c1e52cfcc3f353953a3cf6
Cursos Oficiais Microsoft <moc@proway.treina>
2026-07-23 22:00:29 -0300

    Aula POO 05

---
+++ AulaPOO04/AulaPOO04/Exercicios/Personagem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaPOO04.Exercicios
+{
+    internal class Personagem
+    {
+        public string Nome { get; set; }
+        public int Vida { get; private set; }
+
+        public Personagem(string nome, int vida)
+        {
+            this.Vida = AjustarVida(vida);
+            this.Nome = nome;
+        }
+
+        private int AjustarVida(int vida)
+        {
+            if (vida < 0)
+                return 0;
+            else if (vida > 100)
+                return 100;
+            else
+                return vida;
+        }
+
+        public void ReceberDano(int dano)
+        {
+            this.Vida = AjustarVida(this.Vida - dano);
+        }
+
+        public void Curar(int pontosDeVida)
+        {
+            this.Vida = AjustarVida(this.Vida + pontosDeVida);
+        }
+    }
+}
---
+++ AulaPOO05/AulaPOO05/Exercicios/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaPOO05
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+
+        public double PrecoBase { get; set; }
+
+        public double PorcentagemDesconto { get; set; }
+
+        public double PrecoFinal => PrecoBase - (PorcentagemDesconto / 100 * PrecoBase);
+
+        public Produto(string nome, double precoBase, double porcentagemDesconto)
+        {
+            if (precoBase < 0)
+                throw new ArgumentOutOfRangeException("O valor do produto nao pode ser negativo");
+            if (porcentagemDesconto < 0 || porcentagemDesconto > 50)
+                throw new ArgumentOutOfRangeException(
+                    "O valor da porcentagem nao pode passar do limite de 0-50%"
+                );
+            this.Nome = nome;
+            this.PrecoBase = precoBase;
+            this.PorcentagemDesconto = porcentagemDesconto;
+        }
+    }
+}
