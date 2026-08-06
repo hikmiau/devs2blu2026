35fc3e779bef613cb57e04306322f1c02ebe279a
hikmiau <166746547+hikmiau@users.noreply.github.com>
2026-08-05 21:55:03 -0300

    update

--- DesignPatterns01/DesignPatterns01/Factory/NotificacaoEmail.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DesignPatterns01.Factory;
-
-public class NotificacaoEmail : INotificacao
-{
-    public void Enviar(string mensagem) =>
-        Console.WriteLine("Enviando notificação por e-mail.");
-}
