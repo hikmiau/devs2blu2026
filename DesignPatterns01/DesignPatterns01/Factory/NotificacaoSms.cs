d83ff8a6640476a0828e4cacc4bce4a8a91e5643
hikmiau <166746547+hikmiau@users.noreply.github.com>
2026-08-05 21:55:03 -0300

    update

--- DesignPatterns01/DesignPatterns01/Factory/NotificacaoSms.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DesignPatterns01.Factory;
-
-public class NotificacaoSms : INotificacao
-{
-    public void Enviar(string mensagem) => 
-        Console.WriteLine("Enviando notificação por SMS.");
-}
