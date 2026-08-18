e1357525c8edbb77d12d51b0b072877c63cd0a53
hikmiau <166746547+hikmiau@users.noreply.github.com>
2026-08-18 19:16:35 -0300

    API

---
+++ MinhaPrimeiraApi/MinhaPrimeiraApi/Controllers/ProdutoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaPrimeiraApi.Models;
+
+namespace MinhaPrimeiraApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class ProdutoController : ControllerBase
+{
+    [HttpGet]
+    public string GetTexto() =>
+        "Olá, mundo!";
+}
