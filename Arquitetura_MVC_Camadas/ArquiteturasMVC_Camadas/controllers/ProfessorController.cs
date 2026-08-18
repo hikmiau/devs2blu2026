03f9bbf08d26b0bbba59f1eb805a651e01ee6679
hikmiau <166746547+hikmiau@users.noreply.github.com>
2026-08-17 21:27:24 -0300

    Arquitetura MVC, Camadas

---
+++ Arquitetura_MVC_Camadas/ArquiteturasMVC_Camadas/controllers/ProfessorController.cs
@@ -0,0 +1,53 @@
+﻿using ArquiteturasMVC_Camadas.models;
+using ArquiteturasMVC_Camadas.views;
+
+namespace ArquiteturasMVC_Camadas.controllers;
+
+public class ProfessorController
+{
+    private readonly List<Professor> _repositorioProfessor = new();
+    private readonly ProfessorView _view;
+    
+    public ProfessorController(ProfessorView view)
+    {
+        _view = view;
+    }
+    
+    public void Executar()
+    {
+        bool rodando = true;
+        while (rodando)
+        {
+            int opcao = _view.ExibirMenu();
+            switch (opcao)
+            {
+                case 1:
+                    AdicionarProfessor();
+                    break;
+                case 2:
+                    ListarProfessores();
+                    break;
+                case 0:
+                    rodando = false;
+                    _view.ExibirMensagem("Saindo do sistema...");
+                    break;
+                default:
+                    _view.ExibirMensagem("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+    
+    private void AdicionarProfessor()
+    {
+        Professor novoProfessor = _view.ColetarDados();
+        novoProfessor.Id = _repositorioProfessor.Count + 1;
+        _repositorioProfessor.Add(novoProfessor);
+        _view.ExibirMensagem("Professor cadastrado com sucesso!");
+    }
+    
+    private void ListarProfessores()
+    {
+        _view.ListarProfessores(_repositorioProfessor);
+    }
+}
---
+++ Arquitetura_MVC_Camadas/ArquiteturasMVC_Camadas/views/ProfessorView.cs
@@ -0,0 +1,38 @@
+﻿using ArquiteturasMVC_Camadas.models;
+
+namespace ArquiteturasMVC_Camadas.views;
+
+public class ProfessorView
+{
+    public int ExibirMenu()
+    {
+        Console.WriteLine("\n=== Menu ===\n");
+        Console.WriteLine("1 - Adicionar Professor");
+        Console.WriteLine("2 - Listar Professores");
+        Console.WriteLine("0 - Sair");
+        Console.Write("Escolha uma opção: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    public Professor ColetarDados()
+    {
+        Console.Write("\nNome: ");
+        string nome = Console.ReadLine();
+        Console.Write("Salário: ");
+        double salario = double.Parse(Console.ReadLine());
+        return new Professor { Nome = nome, Salario = salario };
+    }
+
+    public void ExibirMensagem(string mensagem)
+    {
+        Console.WriteLine(mensagem);
+    }
+
+    public void ListarProfessores(List<Professor> professores)
+    {
+        foreach (var professor in professores)
+        {
+            Console.WriteLine($"ID: {professor.Id}, Nome: {professor.Nome}, Salário: {professor.Salario}");
+        }
+    }
+}
