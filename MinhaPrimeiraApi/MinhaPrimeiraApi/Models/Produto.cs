0be868936ba069dc3a9ad387bd58f0a921baf2e3
hikmiau <166746547+hikmiau@users.noreply.github.com>
2026-08-18 19:16:35 -0300

    API

---
+++ MinhaPrimeiraApi/MinhaPrimeiraApi/Models/Produto.cs
@@ -0,0 +1,8 @@
+﻿namespace MinhaPrimeiraApi.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = "";
+    public decimal Preco { get; set; }
+}
