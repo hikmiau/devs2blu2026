71fb692e1275e36070ae491e14441bfcba9484c4
hike <166746547+hikmiau@users.noreply.github.com>
2026-08-26 19:36:55 -0300

    Minha Segunda API

---
+++ MinhaSegundaAPI/MinhaSegundaAPI/Infra/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhaSegundaAPI.Models;
+
+namespace MinhaSegundaAPI.Infra
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Livro> Livros => Set<Livro>();
+    }
+}
---
+++ MinhaSegundaAPI/MinhaSegundaAPI/Models/Livro.cs
@@ -0,0 +1,10 @@
+﻿namespace MinhaSegundaAPI.Models;
+
+public class Livro
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public int AnoPublicacao { get; set; }
+    public decimal Preco { get; set; }
+}
