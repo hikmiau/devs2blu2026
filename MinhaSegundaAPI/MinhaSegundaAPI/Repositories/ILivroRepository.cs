9b763e58583f2e07f948358535ecc5d86adb509e
hikmiau <166746547+hikmiau@users.noreply.github.com>
2026-08-26 19:36:55 -0300

    Minha Segunda API

---
+++ MinhaSegundaAPI/MinhaSegundaAPI/Repositories/ILivroRepository.cs
@@ -0,0 +1,12 @@
+﻿using MinhaSegundaAPI.Models;
+
+namespace MinhaSegundaAPI.Repositories;
+
+public interface ILivroRepository
+{
+    Task<List<Livro>> ObterTodosAsync();
+    Task<Livro?> ObterPorIdAsync(int id);
+    Task AdicionarAsync(Livro livro);
+    Task AtualizarAsync(Livro livro);
+    Task RemoverAsync(Livro livro);
+}
