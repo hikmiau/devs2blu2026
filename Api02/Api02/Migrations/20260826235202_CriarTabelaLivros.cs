f635dce039da50612de918f159adac439037d0da
hikmiau <166746547+hikmiau@users.noreply.github.com>
2026-08-26 21:00:12 -0300

    Api02

---
+++ Api02/Api02/Migrations/20260826235202_CriarTabelaLivros.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Api02.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelaLivros : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Livros",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Titulo = table.Column<string>(type: "text", nullable: false),
+                    Autor = table.Column<string>(type: "text", nullable: false),
+                    AnoPublicacao = table.Column<int>(type: "integer", nullable: false),
+                    Preco = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livros", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Livros");
+        }
+    }
+}
---
+++ Api02/Api02/Models/Livro.cs
@@ -0,0 +1,11 @@
+﻿namespace Api02.Models
+{
+    public class Livro
+    {
+        public int id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Autor { get; set; } = string.Empty;
+        public int AnoPublicacao { get; set; }
+        public decimal Preco {  get; set; }
+    }
+}
