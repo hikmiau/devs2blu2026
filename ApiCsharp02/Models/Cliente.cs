178a3d29224ae86459b6e72c86657ed1d750def3
hikmiau <166746547+hikmiau@users.noreply.github.com>
2026-08-30 20:02:55 -0300

    aidfhisa

--- ApiCsharp02/Models/Cliente.cs
+++ ApiCsharp02/Models/Cliente.cs
@@ -4,6 +4,6 @@
     {
         public Guid? Id { get; set; }
         public string Nome { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty
+        public string Email { get; set; } = string.Empty;
     }
 }
